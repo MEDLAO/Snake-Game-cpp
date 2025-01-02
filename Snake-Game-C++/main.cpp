27addb1d0ccb2c6f27afa2b7608b06852259aa68
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-01-02 13:47:55 +0100

    Declare deque for snake body management

--- Snake-Game-C++/main.cpp
+++ Snake-Game-C++/main.cpp
@@ -6,10 +6,26 @@
 //
 
 #include <iostream>
+#include <deque>
+#include <utility>
 #include <raylib.h>
 
+
+class Snake{
+private:
+    std::deque<std::pair<int, int>> snake;
+public:
+    
+};
+
+
+class Food{
+private:
+public:
+    
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+ 
     return 0;
 }
