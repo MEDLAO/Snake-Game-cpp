8e22ff6d869f0a28d3f99911bd186bee1449614b
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-01-03 19:37:17 +0100

    Add Snake class constructor and draw method

--- Snake-Game-C++/main.cpp
+++ Snake-Game-C++/main.cpp
@@ -11,11 +11,25 @@
 #include <raylib.h>
 
 
+const int screen_width = 1280;
+const int screen_height = 800;
+const int grid_size = 20; // Size of each cell
+const int columns = screen_width / grid_size;
+const int rows = screen_height / grid_size;
+
+
 class Snake{
 private:
     std::deque<std::pair<int, int>> snake;
 public:
-    
+    Snake();
+    void draw(){
+        for (const auto& segment : snake) {
+            int pixelX = segment.first * grid_size;
+            int pixelY = segment.second * grid_size;
+            DrawRectangle(pixelX, pixelY, grid_size, grid_size, GREEN);
+        }
+    }
 };
 
 
@@ -25,17 +39,31 @@ public:
     
 };
 
+Snake::Snake() {
+    int startX = columns / 2;
+    int startY = rows / 2;
+    
+    // Add three initial segments
+    for (int i = 0; i < 3; i++) {
+        snake.push_back({startX - i, startY});
+    }
+}
+
+
+Snake snake;
+
+
 int main(int argc, const char * argv[]) {
-    const int screen_width = 1280;
-    const int screen_height = 800;
     
     InitWindow(screen_width, screen_height, "Snake Game C++");
     SetTargetFPS(60);
     
     while (WindowShouldClose() == false) {
         BeginDrawing();
-        
         ClearBackground(BLUE);
+        
+        snake.draw();
+        
         EndDrawing();
     }
     
